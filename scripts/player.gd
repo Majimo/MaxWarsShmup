2f3794cfeede1cf47013e70565cbf579bfcf4573
Pierre Fervel <pierre.fervel@gmail.com>
2024-07-27 10:28:55 +0200

    Init project and add Player who can shoot bullets

---
+++ scripts/player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+
+var bullet = preload("res://scenes/player_bullet.tscn")
+
+@export var speed := 400
+@export var sprite_half_size := 64
+
+@onready var up_spawn = $SpawnUp
+@onready var down_spawn = $SpawnDown
+
+var can_shoot := true
+
+
+func _process(_delta):
+	if can_shoot and Input.is_action_pressed("ui_accept"):
+		shoot()
+
+func _physics_process(_delta):
+	velocity.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	velocity.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	velocity = velocity.normalized() * speed
+	move_and_slide()
+	global_position.x = clamp(global_position.x, sprite_half_size, get_viewport().size.x - sprite_half_size)
+	global_position.y = clamp(global_position.y, sprite_half_size, get_viewport().size.y - sprite_half_size)
+
+
+func _on_shooting_timer_timeout():
+	can_shoot = true
+
+
+func shoot():
+	can_shoot = false
+	$ShootingTimer.start()
+	
+    # Pas trouvé mieux que de l'instancier deux fois... :/
+	var bullet_instance = bullet.instantiate()
+	get_tree().current_scene.add_child(bullet_instance)
+	bullet_instance.position = up_spawn.global_position
+	
+	var bullet_instance2 = bullet.instantiate()
+	get_tree().current_scene.add_child(bullet_instance2)
+	bullet_instance2.position = down_spawn.global_position
---
+++ scripts/player_bullet.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+@export var speed := 800
+
+
+func _physics_process(_delta):
+	position.x += speed * get_physics_process_delta_time()
+    
+	if global_position.x > (2 * get_viewport().size.y):
+		queue_free()
